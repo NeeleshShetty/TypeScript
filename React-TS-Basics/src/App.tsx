5bf72df2009774ee950ad1f8b9a49c26dbe28c7a
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-05-16 11:42:19 +0530

    props in header

--- React-TS-Basics/src/App.tsx
+++ React-TS-Basics/src/App.tsx
@@ -1,11 +1,21 @@
 import CourseGoal from './component/CourseGoal.tsx'
+
 import './App.css'
+import Header from './component/Header.tsx';
 
 function App() {
   
 
   return (
 		<>
+			<Header
+				image={{
+					src: 'https://img.freepik.com/free-photo/team-together-creating-word-goals_1134-318.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1715644800&semt=ais_user',
+					alt: 'A list of goals',
+				}}
+			>
+				<h1>Your Course Goals</h1>
+			</Header>
 			<CourseGoal title="Learn React + Ts">Learn it from ground up</CourseGoal>
 		</>
 	);
---
+++ React-TS-Basics/src/component/Header.tsx
@@ -0,0 +1,25 @@
+import { type PropsWithChildren } from "react"
+
+type HeaderProps = PropsWithChildren<{
+    image: {
+        src: string;
+        alt: string;
+    }
+}>
+
+const Header = ({image,children}:HeaderProps) => {
+  return (
+		<div>
+			<img
+				src={image.src}
+				alt={image.alt}
+			/>
+			<img
+				{...image}
+			/>
+			{children}
+		</div>
+	);
+}
+
+export default Header
