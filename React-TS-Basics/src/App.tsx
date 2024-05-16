16c3a774b06adf75c177427d688de7a2675607f3
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-05-16 10:47:52 +0530

    setup of Ts and props

---
+++ React-TS-Basics/src/App.tsx
@@ -0,0 +1,16 @@
+import CourseGoal from './component/CourseGoal.tsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <CourseGoal
+        title="Learn React + Ts"
+        description="Learn it from ground up" />
+    </>
+  )
+}
+
+export default App
